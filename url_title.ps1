3fea550a54e79e8a86f10208c20ecca8930926f9
v <zh1024@zohomail.cn>
2023-07-06 17:46:54 +0800

    update

--- url_title.ps1
+++ url_title.ps1
@@ -3,6 +3,6 @@ param([string] $url)
 $wc = New-Object System.Net.WebClient
 $wc.Encoding = [System.Text.Encoding]::UTF8
 $html = $wc.DownloadString($url)
-$html -match '<title .*>(.*?)</title>' | Out-Null;
+$html -match '<title.*>(.*?)</title>' | Out-Null;
 $title = $matches.Count -gt 0 ? $matches[1] : ''
 $title
